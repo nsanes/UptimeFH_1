2d651d4bc62d3f8ef5ebd49f746b848d4ff6527b
Nermina Salkic <ic21b114@technikum-wien.at>
2022-12-21 13:04:59 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/bic/uptimefh/uptimefh/UptimeFhApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.uptimefh.uptimefh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeFhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UptimeFhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/bic/uptimefh/uptimefh/UptimeFhApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.uptimefh.uptimefh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeFhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
