1d2e17e85a32f9da7fd6b1544b6f71437f1c4342
Nermina Salkic <ic21b114@technikum-wien.at>
2022-12-21 13:42:39 +0100

    (this commit will) add UptimeController (new changes).

--- src/main/java/at/fhtw/bic/uptimefh/uptimefh/Controller/UptimeController.java
+++ src/main/java/at/fhtw/bic/uptimefh/uptimefh/Controller/UptimeController.java
@@ -1,7 +1,18 @@
 package at.fhtw.bic.uptimefh.uptimefh.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UptimeController {
+    @GetMapping("/uptime")
+    public double calculateUptime(@RequestParam double relative) {
+        double minutesOfMonth = 60*24*30;
+        double defineUptimeInPercent = relative / 100;
+
+        return minutesOfMonth * defineUptimeInPercent;
+    }
+
+
 }
